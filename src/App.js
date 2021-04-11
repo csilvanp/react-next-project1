be3c74966c049fd1dab75913b1591ea26bd1ef78
Cauã Silva <csilvanp@gmail.com>
2021-04-11 18:29:49 -0300

    Studying lifecycle methods

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Component } from 'react';
 import './App.css';
 class App extends Component {
   state = {
+    counter: 0,
     posts: [
       {
         id: 1,
@@ -19,13 +20,40 @@ class App extends Component {
         body: 'Corpo 3'
       }
     ]
+  };
+
+  timeoutUpdate = null;
+
+  // verifica se o componente foi montado;
+  componentDidMount() {
+    this.handleTimeout();
+  }
+
+  // verifica se o componente foi atualizado;
+  componentDidUpdate() {
+    this.handleTimeout();
+  }
+
+  // verifica se o componente foi desmontado;
+  componentWillUnmount() {
+    clearTimeout(this.timeoutUpdate);
+  }
+
+  handleTimeout = () => {
+    const { posts, counter } = this.state;
+    posts[0].title = 'Título mudou';
+
+    this.timeoutUpdate = setTimeout(() => {
+      this.setState({ posts, counter: counter + 1 });
+    }, 1000);
   }
 
   render() {
-    const { posts } = this.state;
+    const { posts, counter } = this.state;
 
     return (
       <div className="App">
+        <h2>{counter}</h2>
         {posts.map(post => (
           <div key={post.id}>
             <h1>{post.title}</h1>
