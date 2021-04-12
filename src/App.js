f93ce4ad6ed95400fc6511c76a342082507f51f6
Cauã Silva <csilvanp@gmail.com>
2021-04-12 16:34:09 -0300

    Created utils & components folder, refactoring App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import { Component } from 'react';
 import './App.css';
+import { Posts } from './components/Posts';
+import { loadPosts } from './utils/load-posts';
 class App extends Component {
   state = {
     posts: []
   };
 
   // verifica se o componente foi montado;
-  componentDidMount() {
-    this.loadPosts();
+  async componentDidMount() {
+    await this.loadPosts();
   }
 
   loadPosts = async () => {
-    const postsResponse = fetch('https://jsonplaceholder.typicode.com/posts');
-    const photosResponse = fetch('https://jsonplaceholder.typicode.com/photos');
-    const [posts, photos] = await Promise.all([postsResponse, photosResponse]);
-
-    const postsJson = await posts.json();
-    const photosJson = await photos.json();
-
-    const postsAndPhotos = postsJson.map((post, index) => {
-      return { ...post, cover: photosJson[index].url }
-    })
+    const postsAndPhotos = await loadPosts();
     this.setState({ posts: postsAndPhotos });
   }
 
@@ -29,17 +22,7 @@ class App extends Component {
 
     return (
       <section className="container">
-        <div className="posts">
-          {posts.map(post => (
-            <div className="post">
-              <img src={post.cover} alt={post.title} />
-              <div key={post.id} className="post-content">
-                <h1>{post.title}</h1>
-                <p>{post.body}</p>
-              </div>
-            </div>
-          ))}
-        </div>
+        <Posts posts={posts} />
       </section>
     );
   }
---
+++ src/components/Posts/index.jsx
@@ -0,0 +1,15 @@
+import { PostCard } from '../PostCard';
+
+export const Posts = ({ posts }) => {
+  <div className="posts">
+    {posts.map(post => (
+      <PostCard
+        key={post.id}
+        title={post.title}
+        cover={post.cover}
+        body={post.body}
+        id={post.id}
+      />
+    ))}
+  </div>
+}
